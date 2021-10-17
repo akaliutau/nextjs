7996e46f60441842c774ec345bb28ef76249dccd
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2021-10-17 10:08:46 +0000

    updated configuration

--- api/api.js
+++ api/api.js
@@ -1,8 +1,8 @@
 const API_KEY = process.env.UNSPLASH_API_KEY;
 
 const config_data = require('../config.json');
-const london_data = require('../cloud_config/london.json');
-const bristol_data = require('../cloud_config/bristol.json');
+const london_data = require('../cloud_config/config_london.json');
+const bristol_data = require('../cloud_config/config_bristol.json');
 
 const mapping = {
 	'London' : london_data,
