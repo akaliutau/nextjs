a5c90a1d8f7610ea6aeba9f5996164d185283395
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2021-10-18 22:00:53 +0000

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -95,7 +95,7 @@ export default function AboutPage(prop) {
 			</div>
 			<p/>
 			<div>In my spare time I like making photos (some of them are presented on this site), walking uncharted paths, and trying to find new hobby each month.</div>
-			<div>Because you have at least try, and you might be able to do it if you try. But you'll never be able to if you don't. </div>
+			<div>Because you have at least to try, and you might be able to do it if you try. But you'll never be able to if you don't. </div>
 			</div>
 			<SocialFollow/>
 		</main>
